6bcc0c6e164013c338d601801443415930901f3b
Ignacio Parra <ignacioparra57@gmail.com>
2026-02-05 23:16:39 -0800

    added pass hashing and comparison

--- server.js
+++ server.js
@@ -3,7 +3,12 @@ const express = require('express')
 const app = express()
 
 const jwt = require("jsonwebtoken")
-
+const bcrypt = require('bcrypt')
+// the longer the harder to break, but consumes time
+const saltRounds = 10
+const correctPass = "password"
+const wrongPass = "nullpass"
+const testHash = "$2b$10$sYxy7cOwxKLc9JYt/Bb7SeDx80esqT34z7tpo5EtK4zIGExiULFwe"
 posts = [
     {
         username : "Bob",
@@ -23,19 +28,46 @@ app.use(express.json())
 
 // gets post
 app.get('/post', authenticateToken, (req, res) => {
+    // in prod this would be DB query
     res.json(posts.filter(post => post.username === req.user.name))
 })
 
-app.post('/login', (req, res) => {
+app.post('/signup', (req, res) => {
     const username = req.body.username
+    const password = req.body.password
+    // auto gen salt and hash
+    bcrypt.hash(password, saltRounds, function(err, hash) {
+        // Store hash and user in your password DB.
+        if (err) return res.sendStatus(500)
+        res.json({username, hash})
+    });
+})
+
+app.post('/login', (req, res) => {
+    // fetch user from db
+    const username = req.body.username
+    const password = req.body.password
     // serializing user, var : value, attached to accessToken
-    const user = {name : username}
+    const user = { name : username }
+    // also need to collect password
+    // and compare with hash in db
+    async function checkUser(user, password) {
+        // would need to grab hash from db
+        const match = await bcrypt.compare(password, testHash)
 
-    // signing new jwt
-    const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
+        // login
+        if(match) {
+            // signing new jwt
+            const accessToken = jwt.sign(user, process.env.ACCESS_TOKEN_SECRET)
 
-    // login will generate a access token
-    res.json({accessToken : accessToken})
+            // login will generate a access token
+            return res.json({accessToken : accessToken})
+        }
+
+        return res.sendStatus(403)
+    }
+
+    return checkUser(user, password)
 })
 
 function authenticateToken(req, res, next) {
